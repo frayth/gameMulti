5025b920f7f8f73b235fd47a91afbdb3bc1ce87b
frayth <ceresolilauris@gmail.com>
2024-09-16 20:20:23 +0200

    change port

--- back/src/index.ts
+++ back/src/index.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import { usersList } from "./class/usersList";
 export const dotenvConfig=dotenv.config();
 console.log('index',process.env.SECRET_KEY)
-httpServer.listen(3000, '192.168.1.62');
+httpServer.listen(4001, '127.0.0.1');// 3000 ,'192.168.1.62' 
 
 
 setInterval(() => {
