a3f75ef968fbde47c3788a46898bf7efb0afbfbf
frayth <ceresolilauris@gmail.com>
2024-10-10 19:47:03 +0200

    rajout bouton filtre parties accesibles

--- front/gamemulti/src/components/interactive/ListRoom.vue
+++ front/gamemulti/src/components/interactive/ListRoom.vue
@@ -4,35 +4,52 @@
       <input type="text" v-model="filterName" placeholder="Rechercher une partie" />
       <SearchComp :size="30" color="black" :stroke-width="2" class="search-svg" />
     </div>
-
+    <div @click="setJoinableFilter" :style="{width:'40px',justifySelf:'end',alignSelf:'center'}" >
+      <ActivePopUp :message="'Filtrer par parties accessibles'">
+      <div :class="{
+        'select-container':true,
+        'non-active-select':!showFilter,
+        'active-select':showFilter
+      }">
+        <div :class="{ 
+          'ball-container': true,
+          'ball-non-active': !showFilter,
+          'ball-active': showFilter
+         }">
+         <eyeCloseSvg v-if="showFilter" :size="15" color="black" :stroke-width="2" />
+         <EyeOpenSvg v-else :size="15" color="black" />
+        </div>
+      </div>
+    </ActivePopUp>
+    </div>
     <table>
       <thead>
         <tr>
-          <th>
+          <th @click="selectFilter(0, 'th')">
             Nom de la partie
-              <ActivePopUp :message="'trier par nom de partie'" class="chevron-container" >
-                <ChevronSvg 
-                  :size="18"
-                  color="white"
-                  @click="selectFilter(0)"
-                  :class="{
-                    chevron:true,
-                    'active-chevron-incr':
-                      filterActive.index === 0 && filterActive.increment === true,
-                    'active-chevron': filterActive.index === 0 && filterActive.increment === false
-                  }"
-                />
-              </ActivePopUp>
+            <ActivePopUp :message="'trier par nom de partie'" class="chevron-container">
+              <ChevronSvg
+                :size="18"
+                color="white"
+                @click="selectFilter(0, 'svg')"
+                :class="{
+                  chevron: true,
+                  'active-chevron-incr':
+                    filterActive.index === 0 && filterActive.increment === true,
+                  'active-chevron': filterActive.index === 0 && filterActive.increment === false
+                }"
+              />
+            </ActivePopUp>
           </th>
-          <th>
+          <th @click="selectFilter(1, 'th')">
             Nombre de joueurs
             <ActivePopUp :message="'trier par nombre de joueurs'" class="chevron-container">
               <ChevronSvg
                 :size="18"
                 color="white"
-                @click="selectFilter(1)"
+                @click="selectFilter(1, 'svg')"
                 :class="{
-                  chevron:true,
+                  chevron: true,
                   'active-chevron-incr':
                     filterActive.index === 1 && filterActive.increment === true,
                   'active-chevron': filterActive.index === 1 && filterActive.increment === false
@@ -40,15 +57,15 @@
               />
             </ActivePopUp>
           </th>
-          <th>
+          <th @click="selectFilter(0, 'th')">
             Statut
-            <ActivePopUp :message="'trier par statut'"  class="chevron-container">
+            <ActivePopUp :message="'trier par statut'" class="chevron-container">
               <ChevronSvg
                 :size="18"
                 color="white"
-                @click="selectFilter(2)"
+                @click="selectFilter(2, 'svg')"
                 :class="{
-                  chevron:true,
+                  chevron: true,
                   'active-chevron-incr':
                     filterActive.index === 2 && filterActive.increment === true,
                   'active-chevron': filterActive.index === 2 && filterActive.increment === false
@@ -72,7 +89,7 @@
           <td><button @click="joinRoom(room.id)" v-if="room.joinable">Rejoindre</button></td>
         </tr>
       </tbody>
-      <tbody v-else-if="filterName!== '' && listRooms.length!==0 ">
+      <tbody v-else-if="filterName !== '' && listRooms.length !== 0">
         <tr>
           <td colspan="4">Aucune partie ne correspond à votre recherche</td>
         </tr>
@@ -87,8 +104,9 @@
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted, ref} from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import type { GameStatus } from '@/models/room.model'
+import { appliStore } from '@store/appli'
 import peopleSvg from '../../assets/SVG/peopleSvg.vue'
 import { useSocketStore } from '../../stores/socket'
 import { listRooms as roomStore } from '@/stores/Rooms'
@@ -96,34 +114,42 @@ import SearchComp from '@/assets/SVG/SearchComp.vue'
 import ChevronSvg from '@/assets/SVG/ChevronSvg.vue'
 import ActivePopUp from '../UI/ActivePopUp.vue'
 import { storeToRefs } from 'pinia'
-
+import eyeCloseSvg from '@/assets/SVG/eyeCloseSvg.vue'
+import EyeOpenSvg from '@/assets/SVG/EyeOpenSvg.vue'
+const appli = appliStore()
 const socket = useSocketStore()
 const rooms = roomStore()
 const filterName = ref('')
+const showFilter=ref(false)
 const { listRooms } = storeToRefs(rooms)
-const filteredRooms =computed(()=> {
- return rooms.listRooms
- .filter((room) => {
-  return filterName.value===''?true:room.name.toLowerCase().includes(filterName.value.toLowerCase())
-})
- .sort((a, b) => {
-   switch (filterActive.value.index) {
-     case 0:
-       return filterActive.value.increment
-         ? a.name.localeCompare(b.name)
-         : b.name.localeCompare(a.name)
-     case 1:
-       return filterActive.value.increment
-         ? a.players.length - b.players.length
-         : b.players.length - a.players.length
-     case 2:
-       return filterActive.value.increment
-         ? a.status!.localeCompare(b.status!)
-         : b.status!.localeCompare(a.status!)
-     default:
-       return 0
-   }
- })
+const filteredRooms = computed(() => {
+  return rooms.listRooms
+    .filter((room) => {
+      return filterName.value === ''
+        ? true
+        : room.name.toLowerCase().includes(filterName.value.toLowerCase())
+    })
+    .filter((room) => {
+      return showFilter.value ? room.joinable : true
+    })
+    .sort((a, b) => {
+      switch (filterActive.value.index) {
+        case 0:
+          return filterActive.value.increment
+            ? a.name.localeCompare(b.name)
+            : b.name.localeCompare(a.name)
+        case 1:
+          return filterActive.value.increment
+            ? a.players.length - b.players.length
+            : b.players.length - a.players.length
+        case 2:
+          return filterActive.value.increment
+            ? a.status!.localeCompare(b.status!)
+            : b.status!.localeCompare(a.status!)
+        default:
+          return 0
+      }
+    })
 })
 
 const filterActive = ref({
@@ -143,7 +169,6 @@ function ShowStatus(status: GameStatus | null) {
   }
 }
 
-
 onMounted(() => {
   socket.socket?.emit('getRooms:rooms')
 })
@@ -151,18 +176,53 @@ function joinRoom(id: number) {
   if (listRooms.value.find((room) => room.id === id)?.joinable === false) return
   socket.socket?.emit('join:room', { id })
 }
-function selectFilter(index: number) {
+function selectFilter(index: number, element: 'th' | 'svg') {
+  if (appli.getBodyWidth() >= 780 && element === 'th') return
+  if (listRooms.value.length <= 1) return
   if (filterActive.value.index === index) {
     filterActive.value.increment = !filterActive.value.increment
   } else {
-    filterActive.value.increment=true
+    filterActive.value.increment = true
     filterActive.value.index = index
   }
+}
+function setJoinableFilter() {
+  showFilter.value = !showFilter.value
 }
 </script>
 
 <style scoped>
-.chevron-container{
+.select-container {
+position: relative;
+  width: 40px;
+  height: 20px;
+  border-radius: 5px;
+
+}
+.non-active-select {
+  background-color: grey;
+
+}
+.ball-container{
+  position: absolute;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.ball-active{
+  animation: forwards 0.3s ease moveRight;
+}
+.ball-non-active{
+  animation: forwards 0.3s ease moveLeft;
+}
+.active-select {
+  background-color: var(--main-green);
+}
+.chevron-container {
   position: absolute;
   right: 20px;
   top: 50%;
@@ -182,7 +242,7 @@ function selectFilter(index: number) {
   top: 0px;
   opacity: 0.5;
 }
-.test2{
+.test2 {
   width: 30px;
   height: 30px;
 }
@@ -192,9 +252,11 @@ function selectFilter(index: number) {
   border: none;
   outline: none;
   padding-right: 30px;
+  border-radius: 5px;
 }
 .mainRoomList {
   display: grid;
+  grid-template:auto auto / auto auto;
   margin-top: 20px;
 }
 .listRooms {
@@ -234,6 +296,7 @@ table {
   width: 100%;
   border-collapse: collapse;
   margin-top: 10px;
+  grid-column: 1 /span 2;
 }
 td {
   border: 1px solid black;
@@ -258,5 +321,37 @@ th {
 }
 .active-chevron {
   transform: translateY(-50%) rotate(0deg) !important;
+}
+@media (max-width: 780px) {
+  .chevron-container {
+    display: none;
+  }
+  th {
+    cursor: pointer;
+  }
+}
+@media (max-width: 1000px) {
+  .chevron-container {
+    right: 5px;
+  }
+  .mainRoomList {
+    padding: 0 5px;
+  }
+}
+@keyframes moveRight{
+  0%{
+    left: 0px;
+  }
+  100%{
+    left : calc(100% - 22px);
+  }
+}
+@keyframes moveLeft{
+  0%{
+    left : calc(100% - 22px);
+  }
+  100%{
+    left :0
+  }
 }
 </style>
